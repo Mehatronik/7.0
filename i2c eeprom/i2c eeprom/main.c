dba38bd4a3998bd5e3882ff92ea1217c09b21928
Mehatronik <37637174+Mehatronik@users.noreply.github.com>
2018-09-18 22:39:51 +0200

    napredak sa eepromom
    Uspeo da ostvarim potpunu kontorlu nad eepromom preko i2c (TWI). Pisao i brisao po citavom sadrzaju.

--- i2c eeprom/i2c eeprom/main.c
+++ i2c eeprom/i2c eeprom/main.c
@@ -14,8 +14,11 @@
 #include "uart.h"
 #include <util/delay.h>
 
-
-#define atmel24C08A_adr  0b1010000      // device address of EEPROM 24C08a, see datasheet
+//nazovimo ovo page-ovima. Cista adresa eeproma je 10100xxy. xx su bitovi za adresiranje, dok je y read/write bit komanda
+#define atmel24C08A_adr_page0 0b10100000      // device address of EEPROM 24C08a, see datasheet; dodata 0 na kraju radi lakseg citanja i pisanja
+#define atmel24C08A_adr_page1 0b10100010	  //fizicki se koristi samo jedan pin (A1, A2, A3), dok su druga dva nepovezana eksterno, dok se interno
+#define atmel24C08A_adr_page2 0b10100100	  //koriste za adresiranje (10-bit), jer eeprom ima 1024 bajta
+#define atmel24C08A_adr_page3 0b10100110
 
 
 
@@ -24,6 +27,8 @@ int main(void)
 	unsigned char ret;
 	char bafer[15];
 	int i=0;
+	int j=0;
+	uint8_t adr = atmel24C08A_adr_page0; //pomocna prom da bi mogao menjati page-ove
 	
     i2c_init();                                // init I2C interface
 	uart_init();
@@ -34,46 +39,58 @@ int main(void)
 	
 	
 	
-	/* write 93 to eeprom address 0x05 (Byte Write) */
-	 i2c_start_wait( (atmel24C08A_adr << 1) + I2C_WRITE);       // set device address and write mode, last bit is Read-1, Write-0; shift zato sto je addr 7-bit-na
+	
+	 //i2c_start_wait( atmel24C08A_adr_page0 + I2C_WRITE);       
+	 //i2c_write(0x00);
+
+	
+	//prolece kroz sva 4 page-a i svaku adresu. Sve inicijalizuje na zeljenu vrednost - 255 = 0xFF
+	for(i=0; i<4; i++)
+	{	
+		for(j=0; j<256; j++)
+		{
+
+			i2c_start_wait( adr + I2C_WRITE);	// set device address and write mode, last bit is Read-1, Write-0;
 			
-	
-	/* issuing start condition ok, device accessible */
-	//i2c_write(0x01);                       // write address = 0
-	//i2c_write(111);                       // ret=0 -> Ok, ret=1 -> no ACK 
-	i2c_stop();                            // set stop conditon = release bus
-	//PORTB |= 1<<PORTB5; //set led
-               
- 
-	
-	
-	/* podesim pocetnu adresu za citanje */
-	i2c_start_wait( (atmel24C08A_adr << 1) + I2C_WRITE);
-	i2c_write(0x00);										//pocetna adresa = 0
-	
-	
-	
-	
-	for(i=0; i<1024; i++)
-	{
+			i2c_write(j);                       // prvo pisanje je zadavanje adrese (ima ih 256 u jednom page-u)
+			i2c_write(255);						// sledece pisanje je upis vrednosti u tu adresu
+			
+			i2c_stop();							// set stop conditon = release bus
+		}	
+		adr = (adr >> 1) + 1;  //prvo siftujem da bi uvecao page, jer je LSB R/W komanda, a 1 i 2 bit su za adresiranje
+		adr = adr << 1;			//potom vratim da bi bilo 8-bit. LSB se puni nulom
 		
-		
-		i2c_start_wait( (atmel24C08A_adr << 1) + I2C_READ);    //citanje
-		ret = i2c_readNak();                    // read one byte
-		//i2c_stop();
-		
-		
-		/* slanje preko uarta */
-		itoa( ret , bafer, 10);
-		send_str(bafer);
-
-		send_str("\n");  //novi red
-
-		//_delay_ms(20);
-
 	}
 	
 	
+ 
+	
+	/* podesim pocetnu adresu za citanje */
+	i2c_start_wait( atmel24C08A_adr_page0 + I2C_WRITE);
+	i2c_write(0x00);									//pocetna adresa = 0
+	
+	
+	/* ispis na terrminal citavog sadrzaja. 64*16 = 1024. Tako ispisujem jer je takva interna organizacija memorije u eepromu */
+	for(i=0; i<64; i++)
+	{
+		for(j=0; j<16; j++)
+		{
+			i2c_start_wait( atmel24C08A_adr_page0 + I2C_READ);    //citanje
+			ret = i2c_readNak();                    // read one byte
+			
+		
+			/* slanje preko uarta */
+			//itoa( ret , bafer, 10);
+			sprintf(bafer, "%3d", ret);   //presao sam igricu. moze i sprintf umesto itoa. %3d znaci da sam formatirao ispis na minimum 3 mesta, za pregledniji ispis.
+			send_str(bafer);
+
+			send_str("  ");  //razmak
+		}
+		
+		send_str("\n");  //novi red
+	}
+	
+	i2c_stop();
 	
 	
 	
